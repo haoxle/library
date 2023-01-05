8ed93ab242288b412c13a05ca2ca8b57885feb5e
haoxle <hao-le@live.co.uk>
2023-01-05 20:45:34 +0000

    created book and user class

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,90 @@
+package org.example;
+
+public class Book {
+    private int number;
+    private String title;
+    private String author;
+    private String genre;
+    private String subGenre;
+    private String publisher;
+    private boolean onLoan = false;
+    private int amountLoaned = 0;
+
+
+    public Book(int number, String title, String author, String genre, String subGenre, String publisher) {
+        this.number = number;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.subGenre = subGenre;
+        this.publisher = publisher;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getSubGenre() {
+        return subGenre;
+    }
+
+    public void setSubGenre(String subGenre) {
+        this.subGenre = subGenre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public boolean isOnLoan() {
+        return onLoan;
+    }
+
+    public void setOnLoan(boolean onLoan) {
+        this.onLoan = onLoan;
+    }
+
+    public int getAmountLoaned() {
+        return amountLoaned;
+    }
+
+    public void setAmountLoaned(int amountLoaned) {
+        this.amountLoaned = amountLoaned;
+    }
+
+    public void addAmountLoaned() {
+        amountLoaned ++;
+    }
+}
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private boolean admin;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+
+}
